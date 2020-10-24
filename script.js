45410be2daed1816ad25667962eb5cef6d0f91f4
almareb <66138833+almareb@users.noreply.github.com>
2020-10-24 14:22:52 +0400

    update

--- script.js
+++ script.js
@@ -1,46 +1,98 @@
 
-const width = 500;
-const height = 600;
-const grapeD = 50;
 
-let grapeX = width/2;
-let grapeY = height-grapeD/2;
-let platX,platY;
-let rectX, rectY;
-let l = true;
+
+let platform = [];
+var platformNumber = 50;
+var platformGap = 50;
+var grape;
+var r = 0;
+
+function Grape() {
+  this.x = 10;
+  this.y = 10;
+}
+
+function Platform() {
+  this.x = 10;
+  this.y = 10;
+  this.height = 10;
+}
 
 
 function setup() {
-  createCanvas(width, height);
-  
-  
+  createCanvas(800, 500);
+  angleMode(DEGREES);
+
+  //creat platforms
+  for (let i = 0; i < platformNumber; i++) {
+    platform[i] = new Platform();
+    platform[i].x = random(0, 700);
+    platform[i].y = 500 + i * platformGap;
+  }
+
+  //set the starting status of the grape and platform
+  platform[0].x = 0;
+  grape = new Grape();
+  grape.x = platform[0].x + 50;
+  grape.y = platform[0].y - 5;
 }
+
 
 function draw() {
   background(51);
-  grape();
-  if (l == true){
-    platforms();
+  drawPlatform();
+  drawGrape();
+
+  //make the grape move with mouse and stay on the platform/ drop
+  grape.x = mouseX;
+  if (mouseX < platform[r].x || mouseX > platform[r].x + 100) {
+    grape.y = grape.y + 5;
+
+    if (grape.y > platform[r].y + 10) {
+      r++;
+    }
+  } else {
+    grape.y = platform[r].y - 5;
   }
 }
 
 
-function grape(){
-  circle(grapeX, grapeY, grapeD);
-  if (keyIsDown(LEFT_ARROW) && (grapeX-grapeD/2)>=0) {
-    grapeX -= 5;
-  } else if(keyIsDown(RIGHT_ARROW)&& (grapeX+grapeD/2)<=width) {
-    grapeX += 5;
+function drawPlatform() {
+ 
+  fill(111, 38, 189);
+  for (let i = 0; i < platformNumber; i++) {
+    rect(platform[i].x, platform[i].y, 100, platform[i].height, 10);
+    platform[i].y = 500 + i * platformGap - (frameCount / 0.7 % (500 + i * platformGap));
+
+    //score
+    textSize(20);
+    textFont("Rubik Light");
+    text('SCORE:', 625, 30);
+    var score = parseInt(frameCount / 42) + 1;
+    text(score, 715, 30);
   }
 }
 
-function platforms(){
-  
-  for (let i = 0; i < 3; i++) {
-    let platGap = height / 3;
-    rectY = 100 + i * platGap;
-    rectX = random(0, width-150);
-    rect(rectX, rectY, 150,20);
+function drawGrape() {
+  push();
+  translate(grape.x, grape.y);
+  circle(0, 0, 20);
+  fill(79, 25, 70);
+  pop();
+
+
+  //Game over
+  if (grape.y < 0 || grape.y > 500) {
+    stroke(139, 0, 0);
     
+    textFont("Segoe Script");
+    fill(139, 0, 0);
+    textSize(60);
+    textAlign(CENTER);
+    text('GAME OVER', 400, 250);
+    noLoop();
+    noFill();
+    noStroke();
   }
 }
+
